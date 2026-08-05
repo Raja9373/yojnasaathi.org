7b9903351d78ee0f3f0a0721b083ea0a44ee6bbd
Raja9373 <139021048+Raja9373@users.noreply.github.com>
2026-08-06 00:21:18 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
 import AllFaqsPage from "./pages/AllFaqsPage";
-import FaqDetailPage from "./pages/FaqDetailPage";
-import YojanaPage from "./pages/YojanaPage";
-import YojanasPage from "./pages/YojanasPage";
-import BlogPage from "./pages/BlogPage";
+import BlogListPage from "./pages/BlogListPage";
+import BlogDetailPage from "./pages/BlogDetailPage";
+import CategoryPage from "./pages/CategoryPage";
+import ContactPage from "./pages/ContactPage";
+import DisclaimerPage from "./pages/DisclaimerPage";
+import FindYojanaPage from "./pages/FindYojanaPage";
+import HTMLSitemapPage from "./pages/HTMLSitemapPage";
+import LinkCheckerPage from "./pages/LinkCheckerPage";
+import ListingPage from "./pages/ListingPage";
+import PMKisanFAQPage from "./pages/PMKisanFAQPage";
+import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/yojanas" element={<YojanasPage />} />
-        <Route path="/yojana/:slug" element={<YojanaPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        
-        {/* FAQ System - 1000 Pages */}
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/faqs" element={<AllFaqsPage />} />
         <Route path="/faq" element={<AllFaqsPage />} />
-        <Route path="/faq/:slug" element={<FaqDetailPage />} />
+        <Route path="/blog" element={<BlogListPage />} />
+        <Route path="/blog/:slug" element={<BlogDetailPage />} />
+        <Route path="/category/:slug" element={<CategoryPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/disclaimer" element={<DisclaimerPage />} />
+        <Route path="/find-yojana" element={<FindYojanaPage />} />
+        <Route path="/sitemap" element={<HTMLSitemapPage />} />
+        <Route path="/link-checker" element={<LinkCheckerPage />} />
+        <Route path="/yojana/:slug" element={<ListingPage />} />
+        <Route path="/pm-kisan-faq" element={<PMKisanFAQPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
       </Routes>
     </Router>
   );
